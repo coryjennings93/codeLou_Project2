fa16dfd9964ae709ca5fad871194bb53cb5a26e7
Cory Jennings <corydjennings@gmail.com>
2019-07-25 23:15:01 -0400

    slideshow works! now for the buttons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# codeLou_Project2
+# save_the_earth
 
 ------------------------ Save Your Planet ----------------------
 This is the beginning of a website that teaches how pollution is slowly destroying the world, alternatives you can
@@ -6,3 +6,4 @@ take to reduce your carbon footprint, read about movements you can support worki
 via comments.
 
 Open the index.html page to reach the homepage of the website.
+My functioning javaScript is the slideshow towards the bottom of the page.
--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@
 const $button = $('<a class="button" href="#mountain">More Info...</a>');
 $('.moreinfo').append($button);
 
+
+
 //Hide the more info text
 // $('.moreinfo p').hide();
 // when button is pressed show
@@ -34,3 +36,41 @@ $('.moreinfo').on('click', '.button', function(event) {
   // $(this).prev().hide();
   
   // });
+
+const slidePicArray = document.getElementsByClassName('slideImage');
+let currentSlide = 0;
+var slideInterval = setInterval(nextSlide,4000);
+const prevSlideBtn = document.getElementById('prev');
+const nextSlideBtn = document.getElementById('next').textContent;
+
+function nextSlide() {
+  slidePicArray[currentSlide].className = 'slideImage';
+  currentSlide = (currentSlide+1)%slidePicArray.length;
+  slidePicArray[currentSlide].className = 'slideImage show';
+};
+
+
+
+// for (let i = 0; i < slidePicArray.length; i += 1) {
+//    slidePicArray[i].className = "show";
+//    slideInterval();
+
+// };
+
+
+
+// for (let i = 0; i < slidePicArray.length; i += 1) {
+//   slidePicArray[i].style.display = "block"
+//   // If you go over all slides, restart the index to show the first slide and start again
+//   if (i === slides.length) index = 0; 
+      
+//   slides[index].classList.add('active');
+
+// }, 2000)
+
+// };
+// };
+
+
+
+
